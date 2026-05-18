2fe6cad51e2932f313649ea54fde4f8f73694997
Carlos QU <45314580+CCandle@users.noreply.github.com>
2026-05-19 00:50:06 +0800

    fix: deduplicate treesitter plugin wiring

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,55 +1,35 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
 	lazy = false,
-	dependencies = {
-		"nvim-treesitter/nvim-treesitter-context",
-		"HiPhish/rainbow-delimiters.nvim",
-	},
-
 	config = function()
 		pcall(vim.treesitter.language.register, "bibtex", "bib")
 		pcall(vim.treesitter.language.register, "systemverilog", { "verilog", "systemverilog" })
 
-		require("nvim-treesitter.configs").setup({
-			auto_install = true,
-			ensure_installed = {
-				"c",
-				"cpp",
-				"python",
-				"lua",
-				"vim",
-				"vimdoc",
-				"query",
-				"bibtex",
-				"markdown",
-				"markdown_inline",
-				"systemverilog",
-				"vhdl",
-				"matlab",
-				"html",
-				"css",
-				"javascript",
-			},
-			highlight = { enable = true },
-			indent = { enable = true },
-		})
-
-		vim.g.rainbow_delimiters = {
-			strategy = {
-				[""] = require("rainbow-delimiters").strategy["global"],
-			},
-			query = {
-				[""] = "rainbow-delimiters",
-			},
-			highlight = {
-				"RainbowDelimiterRed",
-				"RainbowDelimiterYellow",
-				"RainbowDelimiterBlue",
-				"RainbowDelimiterOrange",
-				"RainbowDelimiterGreen",
-				"RainbowDelimiterViolet",
-				"RainbowDelimiterCyan",
-			},
-		}
+		local ok, ts = pcall(require, "nvim-treesitter.configs")
+		if ok then
+			ts.setup({
+				auto_install = true,
+				ensure_installed = {
+					"c",
+					"cpp",
+					"python",
+					"lua",
+					"vim",
+					"vimdoc",
+					"query",
+					"bibtex",
+					"markdown",
+					"markdown_inline",
+					"systemverilog",
+					"vhdl",
+					"matlab",
+					"html",
+					"css",
+					"javascript",
+				},
+				highlight = { enable = true },
+				indent = { enable = true },
+			})
+		end
 	end,
 }
