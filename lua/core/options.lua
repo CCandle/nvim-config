634e451be3c13763b2a37598cf2cc7956fbe8eeb
Lingfeng QU <Carlos_QU@163.com>
2026-08-14 17:28:53 +0800

    refactor: use code-first global options and enable trusted project config

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,34 +1,35 @@
 local opt = vim.opt
 
--- 行号
-opt.relativenumber = true
 opt.number = true
+opt.relativenumber = true
+opt.cursorline = true
+opt.signcolumn = "yes"
 
--- 缩进
 opt.tabstop = 2
 opt.shiftwidth = 2
 opt.expandtab = true
 opt.autoindent = true
 opt.breakindent = true
 
--- wrap
-opt.wrap = true
-
-opt.cursorline = true
+-- Code is the conservative global default; prose filetypes opt back into wrapping
+-- from after/ftplugin.
+opt.wrap = false
 
 opt.mouse:append("a")
-
 opt.clipboard:append("unnamedplus")
 
 opt.ignorecase = true
 opt.smartcase = true
 opt.termguicolors = true
-opt.signcolumn = "yes"
 opt.splitright = true
 opt.splitbelow = true
+opt.splitkeep = "screen"
 opt.completeopt = { "menu", "menuone", "noselect" }
 opt.updatetime = 250
 opt.timeoutlen = 500
 opt.undofile = true
-
 opt.scrolloff = 5
+opt.confirm = true
+
+-- Project-local .nvim.lua files are still gated by Neovim's trust database.
+opt.exrc = true
