fde68daf56d732dd90e6931b91c92781ab2f1276
Lingfeng QU <Carlos_QU@163.com>
2026-08-14 17:35:52 +0800

    experiment: replace ad-hoc embedded terminals with Overseer MCU tasks

--- lua/plugins/embedded.lua
+++ lua/plugins/embedded.lua
@@ -1,33 +1,29 @@
 return {
   {
-    "akinsho/toggleterm.nvim",
+    "stevearc/overseer.nvim",
+    cmd = { "OverseerOpen", "OverseerToggle", "OverseerRun", "OverseerTaskAction" },
     keys = {
-      {
-        "<leader>mc",
-        function()
-          local root = vim.fs.root(0, { "CMakeLists.txt", ".git" }) or vim.uv.cwd()
-          local Terminal = require("toggleterm.terminal").Terminal
-          Terminal:new({
-            cmd = "cd " .. vim.fn.shellescape(root) .. " && cmake -B build",
-            direction = "float",
-            close_on_exit = false,
-          }):toggle()
-        end,
-        desc = "CMake configure",
-      },
-      {
-        "<leader>mt",
-        function()
-          local root = vim.fs.root(0, { "CMakeLists.txt", "Makefile", ".git" }) or vim.uv.cwd()
-          local Terminal = require("toggleterm.terminal").Terminal
-          Terminal:new({
-            cmd = "cd " .. vim.fn.shellescape(root) .. " && ctest --test-dir build",
-            direction = "float",
-            close_on_exit = false,
-          }):toggle()
-        end,
-        desc = "Run tests (ctest)",
+      { "<leader>mc", function() require("core.mcu").configure() end, desc = "MCU CMake configure" },
+      { "<leader>mb", function() require("core.mcu").build() end, desc = "MCU build" },
+      { "<leader>mC", function() require("core.mcu").clean() end, desc = "MCU clean" },
+      { "<leader>mf", function() require("core.mcu").flash() end, desc = "MCU flash" },
+      { "<leader>mo", function() require("core.mcu").openocd() end, desc = "MCU OpenOCD" },
+      { "<leader>mr", function() require("core.mcu").reset() end, desc = "MCU reset" },
+      { "<leader>md", function() require("core.mcu").debug() end, desc = "MCU debug" },
+      { "<leader>ms", function() require("core.mcu").status() end, desc = "MCU config status" },
+      { "<leader>mx", "<cmd>OverseerToggle<CR>", desc = "MCU task list" },
+    },
+    opts = {
+      dap = true,
+      task_list = {
+        direction = "bottom",
+        min_height = 8,
+        max_height = { 18, 0.3 },
       },
     },
+    config = function(_, opts)
+      require("overseer").setup(opts)
+      require("which-key").add({ { "<leader>m", group = "+MCU / embedded" } })
+    end,
   },
 }
