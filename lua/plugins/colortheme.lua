9defb16501326265113d22739ddb897b30c08b3c
Lingfeng QU <Carlos_QU@163.com>
2026-08-14 17:29:16 +0800

    feat: make Nightfox transparency configurable

--- lua/plugins/colortheme.lua
+++ lua/plugins/colortheme.lua
@@ -1,8 +1,15 @@
+local settings = require("core.settings")
+
 return {
   {
     "EdenEast/nightfox.nvim",
-    priority = 1000, -- 必须高，保证先加载
+    priority = 1000,
     config = function()
+      require("nightfox").setup({
+        options = {
+          transparent = settings.ui.transparent,
+        },
+      })
       vim.cmd("colorscheme nightfox")
     end,
   },
