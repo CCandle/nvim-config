3adc8a72e99a2c63fb4421eb214a526e7eab78e3
Lingfeng QU <Carlos_QU@163.com>
2026-08-14 17:38:41 +0800

    experiment: keep Oil deletion dependency-free

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -10,7 +10,7 @@ return {
     opts = {
       default_file_explorer = false,
       columns = { "icon" },
-      delete_to_trash = true,
+      delete_to_trash = false,
       skip_confirm_for_simple_edits = false,
       view_options = {
         show_hidden = true,
