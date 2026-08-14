e9a913c9c7231a8206666dfda1c2126a06afa9e2
Lingfeng QU <Carlos_QU@163.com>
2026-08-14 17:27:44 +0800

    fix: migrate Tree-sitter to current main API and add textobjects

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,35 +1,106 @@
-return {
-	"nvim-treesitter/nvim-treesitter",
-	lazy = false,
-	config = function()
-		pcall(vim.treesitter.language.register, "bibtex", "bib")
-		pcall(vim.treesitter.language.register, "systemverilog", { "verilog", "systemverilog" })
+local parsers = {
+  "c",
+  "cpp",
+  "python",
+  "lua",
+  "vim",
+  "vimdoc",
+  "query",
+  "bibtex",
+  "markdown",
+  "markdown_inline",
+  "systemverilog",
+  "vhdl",
+  "matlab",
+  "html",
+  "css",
+  "javascript",
+}
 
-		local ok, ts = pcall(require, "nvim-treesitter.configs")
-		if ok then
-			ts.setup({
-				auto_install = true,
-				ensure_installed = {
-					"c",
-					"cpp",
-					"python",
-					"lua",
-					"vim",
-					"vimdoc",
-					"query",
-					"bibtex",
-					"markdown",
-					"markdown_inline",
-					"systemverilog",
-					"vhdl",
-					"matlab",
-					"html",
-					"css",
-					"javascript",
-				},
-				highlight = { enable = true },
-				indent = { enable = true },
-			})
-		end
-	end,
+local highlighted_filetypes = {
+  "c",
+  "cpp",
+  "python",
+  "lua",
+  "vim",
+  "help",
+  "query",
+  "bib",
+  "markdown",
+  "systemverilog",
+  "verilog",
+  "vhdl",
+  "matlab",
+  "html",
+  "css",
+  "javascript",
+}
+
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    lazy = false,
+    build = ":TSUpdate",
+    config = function()
+      pcall(vim.treesitter.language.register, "bibtex", "bib")
+      pcall(vim.treesitter.language.register, "systemverilog", { "verilog", "systemverilog" })
+
+      require("nvim-treesitter").install(parsers)
+
+      local group = vim.api.nvim_create_augroup("CarlosTreesitter", { clear = true })
+      vim.api.nvim_create_autocmd("FileType", {
+        group = group,
+        pattern = highlighted_filetypes,
+        callback = function(args)
+          pcall(vim.treesitter.start, args.buf)
+        end,
+      })
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    branch = "main",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    config = function()
+      require("nvim-treesitter-textobjects").setup({
+        select = {
+          lookahead = true,
+          selection_modes = {
+            ["@parameter.outer"] = "v",
+            ["@function.outer"] = "V",
+          },
+        },
+        move = { set_jumps = true },
+      })
+
+      local select = require("nvim-treesitter-textobjects.select")
+      local move = require("nvim-treesitter-textobjects.move")
+
+      vim.keymap.set({ "x", "o" }, "af", function()
+        select.select_textobject("@function.outer", "textobjects")
+      end, { desc = "Around function" })
+      vim.keymap.set({ "x", "o" }, "if", function()
+        select.select_textobject("@function.inner", "textobjects")
+      end, { desc = "Inside function" })
+      vim.keymap.set({ "x", "o" }, "aa", function()
+        select.select_textobject("@parameter.outer", "textobjects")
+      end, { desc = "Around argument" })
+      vim.keymap.set({ "x", "o" }, "ia", function()
+        select.select_textobject("@parameter.inner", "textobjects")
+      end, { desc = "Inside argument" })
+
+      vim.keymap.set({ "n", "x", "o" }, "]f", function()
+        move.goto_next_start("@function.outer", "textobjects")
+      end, { desc = "Next function" })
+      vim.keymap.set({ "n", "x", "o" }, "[f", function()
+        move.goto_previous_start("@function.outer", "textobjects")
+      end, { desc = "Previous function" })
+      vim.keymap.set({ "n", "x", "o" }, "]a", function()
+        move.goto_next_start("@parameter.inner", "textobjects")
+      end, { desc = "Next argument" })
+      vim.keymap.set({ "n", "x", "o" }, "[a", function()
+        move.goto_previous_start("@parameter.inner", "textobjects")
+      end, { desc = "Previous argument" })
+    end,
+  },
 }
